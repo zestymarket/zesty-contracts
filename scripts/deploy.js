c6746facc6938f11eb0d2aeba634346856c663bf
jseam2 <joshia_seam@mymail.sutd.edu.sg>
2021-02-12 18:49:16 +0800

    update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -22,10 +22,16 @@ async function main() {
   const ZestyToken = await hre.ethers.getContractFactory("ZestyToken");
   const zestyToken = await ZestyToken.deploy();
   await zestyToken.deployed();
-
   console.log("ZestyToken deployed to:", zestyToken.address);
 
-
+  const AuctionHTLC = await hre.ethers.getContractFactory("AuctionHTLC");
+  const auctionHTLC = await AuctionHTLC.deploy(
+    zestyToken.address,
+    zestyNFT.address,
+    "0x706400D5200f49D89F016cfbBB90Bb94668Fc80c"
+  );
+  await auctionHTLC.deployed();
+  console.log("AuctionHTLC deployed to:", auctionHTLC.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
